91375b3cba8ca65ddbc95e244c7f8ba78cfe9932
Geir Arne Hjelle <geirarne@gmail.com>
2016-12-06 23:11:51 +0100

    Use an infinite generator for puzzle 10 2015

--- 2015/10_elves_look_elves_say/aoc10.py
+++ 2015/10_elves_look_elves_say/aoc10.py
@@ -7,12 +7,10 @@ import itertools
 import sys
 
 
-def look_and_say(sequence, repeat):
-    next_seq = ''.join(str(len(list(g[1]))) + g[0] for g in itertools.groupby(sequence))
-    if repeat > 1:
-        return [len(next_seq)] + look_and_say(next_seq, repeat-1)
-
-    return [len(next_seq)]
+def look_and_say(sequence):
+    while True:
+        sequence = ''.join(str(len(list(g[1]))) + g[0] for g in itertools.groupby(sequence))
+        yield sequence
 
 
 def main():
@@ -20,10 +18,9 @@ def main():
         print('\n{}:'.format(filename))
         with open(filename, mode='r') as fid:
             for line in fid:
-                seed = line.strip()
-                lengths = look_and_say(seed, repeat=50)
-                print('Length after 40 iterations: {}'.format(lengths[39]))
-                print('Length after 50 iterations: {}'.format(lengths[49]))
+                lengths = [len(s) for s in itertools.islice(look_and_say(line.strip()), 50)]
+                print('Length of {} after 40 iterations: {}'.format(line.strip(), lengths[39]))
+                print('Length of {} after 50 iterations: {}'.format(line.strip(), lengths[49]))
 
 
 if __name__ == '__main__':
