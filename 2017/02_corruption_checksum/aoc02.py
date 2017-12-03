c14de457d192753c5d107e91fcdd4bc5bad69ae4
Geir Arne Hjelle <geirarne@gmail.com>
2017-12-03 22:54:54 +0100

    Puzzle 2 2017

---
+++ 2017/02_corruption_checksum/aoc02.py
@@ -0,0 +1,42 @@
+"""Corruption Checksum
+
+Advent of code 2017, day 2
+Solution by Geir Arne Hjelle, 2017-12-02
+"""
+import sys
+import numpy as np
+
+
+def read_spreadsheet(filename):
+    sheet = []
+    with open(filename, mode='r') as fid:
+        for line in fid:
+            sheet.append([int(n) for n in line.strip().split()])
+
+    return sheet
+
+
+def calculate_rowsum(sheet):
+    return sum([(max(row) - min(row)) for row in sheet])
+
+
+def calculate_divisible_sum(sheet):
+    total = 0
+    for row in sheet:
+        div = np.array([[divmod(r, c)[0] for c in row] for r in row])
+        mod = np.array([[divmod(r, c)[1] for c in row] for r in row])
+        mod += np.eye(len(mod), dtype=int)
+        total += div[mod == 0][0]
+    return total
+
+
+def main():
+    for filename in sys.argv[1:]:
+        print('\n{}:'.format(filename))
+        sheet = read_spreadsheet(filename)
+        print(calculate_rowsum(sheet))
+        print(calculate_divisible_sum(sheet))
+
+
+if __name__ == '__main__':
+    main()
