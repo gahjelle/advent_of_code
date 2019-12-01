56ef561262d8f79f27108bcfdc8e8b3f8399c959
Geir Arne Hjelle <geirarne@gmail.com>
2019-12-01 12:29:50 +0100

    Puzzle 1 2019

---
+++ 2019/01_the_tyranny_of_the_rocket_equation/aoc01.py
@@ -0,0 +1,43 @@
+"""The Tyranny of the Rocket Equation
+
+Advent of Code 2019, day 1
+Solution by Geir Arne Hjelle, 2019-12-01
+"""
+import sys
+
+
+def fuel(mass):
+    """Calculate fuel based on mass"""
+    return max(mass // 3 - 2, 0)
+
+
+def recursive_fuel(mass):
+    """Calculate fuel based on mass, include fuel to cover mass of fuel"""
+    fuel = mass // 3 - 2
+    if fuel <= 0:
+        return 0
+
+    return fuel + recursive_fuel(fuel)
+
+
+def main():
+    for filename in sys.argv[1:]:
+        if filename.startswith("--"):
+            continue
+
+        print(f"\n{filename}:")
+
+        # Part 1
+        with open(filename, mode="r") as fid:
+            total_fuel = sum(fuel(int(line)) for line in fid)
+        print(f"Total fuel:  {total_fuel}")
+
+        # Part 2
+        with open(filename, mode="r") as fid:
+            rec_fuel = sum(recursive_fuel(int(line)) for line in fid)
+        print(f"Total fuel, including fuel for fuel:  {rec_fuel}")
+
+
+if __name__ == "__main__":
+    debug = print if "--debug" in sys.argv else lambda *_: None
+    main()
