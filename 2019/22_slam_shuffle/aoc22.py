38fd0fadec6e80371e4dcbfa4720378b548c5de3
Geir Arne Hjelle <geirarne@gmail.com>
2019-12-22 15:44:14 +0100

    Puzzle 22 2019

---
+++ 2019/22_slam_shuffle/aoc22.py
@@ -0,0 +1,126 @@
+"""Slam Shuffle
+
+Advent of Code 2019, day 22
+Solution by Geir Arne Hjelle, 2019-12-22
+"""
+import pathlib
+import sys
+
+import parse
+
+debug = print if "--debug" in sys.argv else lambda *_: None
+
+
+def deal_into_new_stack(card_pos):
+    return -card_pos - 1
+
+
+def cut_cards(card_pos, num):
+    return card_pos - num
+
+
+def deal_with_increment(card_pos, num):
+    return card_pos * num
+
+
+COMMANDS = {
+    parse.compile("deal into new stack"): deal_into_new_stack,
+    parse.compile("cut {num:d}"): cut_cards,
+    parse.compile("deal with increment {num:d}"): deal_with_increment,
+}
+
+
+def update_num_cards(shuffles, default):
+    pattern = parse.compile("use {num:d} cards")
+
+    num_cards = default
+    for shuffle in shuffles:
+        result = pattern.parse(shuffle)
+        if result:
+            num_cards = result.named["num"]
+
+    return num_cards
+
+
+def translate_shuffles(shuffles):
+    commands = []
+    for shuffle in shuffles:
+        debug(shuffle)
+        for pattern, command in COMMANDS.items():
+            result = pattern.parse(shuffle)
+            if result:
+                commands.append((command, result.named))
+    return commands
+
+
+def as_linear(commands):
+    maps = {}
+    for card in range(2):
+        card_pos = card
+        for command, args in commands:
+            card_pos = command(card_pos, **args)
+        maps[card] = card_pos
+
+    return maps[1] - maps[0], maps[0]
+
+
+def geom_sum(a, n, m):
+    """Calculate $a^{n-1} + ... + a + 1 mod m$
+
+    https://stackoverflow.com/a/42033401
+    """
+    total = 0
+    s = 1
+    while n > 0:
+        if n % 2:
+            total = (a * total + s) % m
+        s = (s * (a + 1)) % m
+        n = n // 2
+        a = (a * a) % m
+
+    return total
+
+
+def shuffle_deck(coeffs, card, num_cards, repeats, inverse=False):
+    a, b = [c % num_cards for c in coeffs]
+    if inverse:
+        # y(c) = ac + b => ŷ(p) = âp - âb with ^ as inverse
+        a_inv = pow(a, -1, num_cards)
+        a, b = a_inv % num_cards, (-a_inv * b) % num_cards
+
+    a_n = pow(a, repeats, num_cards)
+    s_n = geom_sum(a, repeats, num_cards)
+    debug(card, a, b, a_n, s_n)
+
+    return (a_n * card + s_n * b) % num_cards
+
+
+def main(args):
+    for file_path in [pathlib.Path(p) for p in args if not p.startswith("-")]:
+        print(f"\n{file_path}:")
+        shuffles = file_path.read_text().strip().split("\n")
+        commands = translate_shuffles(shuffles)
+        coeffs = as_linear(commands)
+        debug(coeffs)
+
+        # Part 1
+        num_cards = update_num_cards(shuffles, default=10007)
+        card = 2019 % num_cards
+        part_1 = shuffle_deck(coeffs, card=card, num_cards=num_cards, repeats=1)
+        print(f"Card {card} is in position {part_1}")
+
+        # Part 2
+        num_cards = update_num_cards(shuffles, default=119315717514047)
+        card_pos = 2020 % num_cards
+        part_2 = shuffle_deck(
+            coeffs,
+            card=card_pos,
+            num_cards=num_cards,
+            repeats=101741582076661,
+            inverse=True,
+        )
+        print(f"Card {part_2} is in position {card_pos}")
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
