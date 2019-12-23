282905b01539a47d1c3827ff5aa9ff4e736a7f79
Geir Arne Hjelle <geirarne@gmail.com>
2019-12-23 11:53:47 +0100

    Update puzzle 5 2015

--- 2015/05_doesnt_he_have_intern-elves_for_this/aoc05.py
+++ 2015/05_doesnt_he_have_intern-elves_for_this/aoc05.py
@@ -5,19 +5,25 @@ Solution by Geir Arne Hjelle, 2016-12-03
 """
 import sys
 
-
-def find_nice_strings_1(fid):
-    return [s.strip() for s in fid if check_vowels(s) and check_double(s) and check_naughty(s)]
+debug = print if "--debug" in sys.argv else lambda *_: None
 
 
-def find_nice_strings_2(fid):
-    return [s.strip() for s in fid if check_twice(s) and check_repeats(s)]
+def find_nice_strings_1(strings):
+    return [
+        s.strip()
+        for s in strings
+        if check_vowels(s) and check_double(s) and check_naughty(s)
+    ]
+
+
+def find_nice_strings_2(strings):
+    return [s.strip() for s in strings if check_twice(s) and check_repeats(s)]
 
 
 def check_vowels(string):
     """Check if string contains at least three vowels
     """
-    return sum(string.count(v) for v in 'aeiou') >= 3
+    return sum(string.count(v) for v in "aeiou") >= 3
 
 
 def check_double(string):
@@ -32,21 +38,23 @@ def check_double(string):
 def check_naughty(string):
     """Check that string does not contain the strings 'ab', 'cd', 'pq', or 'xy'
     """
-    return not any(s in string for s in ('ab', 'cd', 'pq', 'xy'))
+    return not any(s in string for s in {"ab", "cd", "pq", "xy"})
 
 
 def check_twice(string):
-    """Check if string contains a pair of any two letters that appears at least twice without overlapping
+    """Check if string contains a pair of any two letters that appears at least
+       twice without overlapping
     """
     for i in range(len(string) - 2):
-        l, c, r = string[:i], string[i:i+2], string[i+2:]
-        if c in l or c in r:
+        c, r = string[i : i + 2], string[i + 2 :]
+        if c in r:
             return True
     return False
 
 
 def check_repeats(string):
-    """Check if string contains at least one letter which repeats with exactly one letter between
+    """Check if string contains at least one letter which repeats with exactly
+       one letter between
     """
     for c1, c2 in zip(string[:-2], string[2:]):
         if c1 == c2:
@@ -54,19 +62,25 @@ def check_repeats(string):
     return False
 
 
-def main():
-    for filename in sys.argv[1:]:
-        print('\n{}:'.format(filename))
-        with open(filename, mode='r') as fid:
-            nice_strings = find_nice_strings_1(fid)
-            print('{} strings are nice according to the old rules\n  ({})'
-                  ''.format(len(nice_strings), ', '.join(nice_strings)))
+def main(args):
+    for filename in args:
+        if filename.startswith("-"):
+            continue
 
-        with open(filename, mode='r') as fid:
-            nice_strings = find_nice_strings_2(fid)
-            print('{} strings are nice according to the new rules\n  ({})'
-                  ''.format(len(nice_strings), ', '.join(nice_strings)))
+        print(f"\n{filename}:")
+        with open(filename, mode="r") as fid:
+            strings = fid.readlines()
+
+        # Part 1
+        nice_strings = find_nice_strings_1(strings)
+        print(f"{len(nice_strings)} strings are nice according to the old rules")
+        debug(", ".join(sorted(nice_strings)))
+
+        # Part 2
+        nice_strings = find_nice_strings_2(strings)
+        print(f"{len(nice_strings)} strings are nice according to the new rules")
+        debug(", ".join(sorted(nice_strings)))
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    main(sys.argv[1:])
