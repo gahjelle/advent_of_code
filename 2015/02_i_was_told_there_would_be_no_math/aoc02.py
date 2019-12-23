a286b5f551483bf75f65ad094b2956064a879033
Geir Arne Hjelle <geirarne@gmail.com>
2019-12-23 11:22:59 +0100

    Update puzzle 2 2015

--- 2015/02_i_was_told_there_would_be_no_math/aoc02.py
+++ 2015/02_i_was_told_there_would_be_no_math/aoc02.py
@@ -11,25 +11,25 @@ def calculate(fid):
     ribbon = list()
 
     for line in fid:
-        w, h, d = sorted([int(n) for n in line.split('x')])
+        w, h, d = sorted([int(n) for n in line.split("x")])
         wrap_area = 2 * (w * h + w * d + h * d) + w * h
         ribbon_length = 2 * (w + h) + w * h * d
         wrapping.append(wrap_area)
         ribbon.append(ribbon_length)
 
-        # print('{:<10s}: The elves need {:5d} ft² of wrapping paper and {:5d} ft of ribbon'
-        #       ''.format(line.strip(), wrap_area, ribbon_length))
-
     return wrapping, ribbon
 
-def main():
-    for filename in sys.argv[1:]:
-        print('\n{}:'.format(filename))
-        with open(filename, mode='r') as fid:
+
+def main(args):
+    for filename in args:
+        print(f"\n{filename}:")
+        with open(filename, mode="r") as fid:
             wrapping, ribbon = calculate(fid)
-            print('In total, the elves need {} ft² of wrapping paper and {} ft of ribbon'
-                  ''.format(sum(wrapping), sum(ribbon)))
+            print(
+                f"In total, the elves need {sum(wrapping)} ft² of wrapping paper "
+                f"and {sum(ribbon)} ft of ribbon"
+            )
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    main(sys.argv[1:])
