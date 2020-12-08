fd744c1b1754518829693dbd69cba61887128d1e
Geir Arne Hjelle <geirarne@gmail.com>
2020-12-08 07:05:20 +0100

    I like pipes :)

--- 2019/01_the_tyranny_of_the_rocket_equation/aoc01.jl
+++ 2019/01_the_tyranny_of_the_rocket_equation/aoc01.jl
@@ -3,23 +3,18 @@
 # Advent of Code 2019, day 1
 # Solution by Geir Arne Hjelle, 2020-11-30
 
-"""
-    fuel(mass)
+using Pipe
 
-Calculate fuel based on mass
-"""
+# Calculate fuel based on mass
 function fuel(mass)
-    return Int64(floor(mass / 3) - 2)
+    floor(mass / 3) - 2 |> Int64
 end
 
 
-"""
-    recursive_fuel(mass)
-
-Calculate fuel based on mass, include fuel to cover mass of fuel
-"""
+# Calculate fuel based on mass, include fuel to cover mass of fuel
 function recursive_fuel(mass)
-    fuel_for_mass = fuel(mass)
+    fuel_for_mass = mass |> fuel
+
     if fuel_for_mass <= 0
         return 0
     else
@@ -28,28 +23,22 @@ function recursive_fuel(mass)
 end
 
 
-"""
-    main(filename)
-
-Solve the problem for one file
-"""
-function main(filename)
+# Solve the problem for one file
+function solve(filename)
     println("\n$(filename)")
 
     # Read from file
     input = open(filename) do fid
-        parse.(Int64, readlines(fid))
+        @pipe fid |> readlines .|> parse(Int64, _)
     end
 
     # Part 1
-    total_fuel = sum(fuel.(input))
-    println("Total fuel:  $(total_fuel)")
+    input .|> fuel |> sum |> println
 
     # Part 2
-    rec_fuel = sum(recursive_fuel.(input))
-    println("Total fuel, including fuel for fuel:  $(rec_fuel)")
+    input .|> recursive_fuel |> sum |> println
 end
 
 
-# Run main on each file
-main.(ARGS)
+# Run solve on each file
+ARGS .|> solve
