516807bd167b602d1c349373ac74be47dbf7a359
Geir Arne Hjelle <geirarne@gmail.com>
2020-12-11 00:24:51 +0100

    Use caching to speed up bag search

--- 2020/07_handy_haversack/aoc07.py
+++ 2020/07_handy_haversack/aoc07.py
@@ -3,45 +3,46 @@
 Advent of Code 2020, day 7
 Solution by Geir Arne Hjelle, 2020-12-07
 """
+import functools
 import pathlib
 import re
 import sys
 
 BAGS = re.compile(r"(?P<outer>.+) bags contain (?P<inner>.+).")
 BAGS_INNER = re.compile(r"(?P<num>\d+) (?P<color>[\w ]+) bags?")
+BAG_TREE = {}
 
 debug = print if "--debug" in sys.argv else lambda *_: None
 
 
 def build_bag_tree(lines):
     """Build a tree of bags containing each other"""
-    bags = {}
     for line in lines:
         match = BAGS.match(line).groupdict()
         outer, inner_bags = match["outer"], match["inner"].split(", ")
-        bags.setdefault(outer, dict())
+        BAG_TREE.setdefault(outer, dict())
         for inner in inner_bags:
             match = BAGS_INNER.match(inner)
             if match:
                 bag = match.groupdict()
-                bags[outer][bag["color"]] = int(bag["num"])
-
-    return bags
+                BAG_TREE[outer][bag["color"]] = int(bag["num"])
 
 
-def can_contain(bags, color):
+@functools.lru_cache
+def can_contain(color):
     """Find bags that can eventually contain the given color"""
-    directly = {b for b, c in bags.items() if color in c}
-    inside = [can_contain(bags, c) for c in directly]
+    directly = {b for b, c in BAG_TREE.items() if color in c}
+    inside = [can_contain(c) for c in directly]
     if inside:
-        return directly | set.union(*[can_contain(bags, c) for c in directly])
+        return directly | set.union(*[can_contain(c) for c in directly])
     else:
         return directly
 
 
-def must_contain(bags, color):
+@functools.lru_cache
+def must_contain(color):
     """Find number of bags that must be contained within a bag of the given color"""
-    return 1 + sum(must_contain(bags, c) * n for c, n in bags[color].items())
+    return 1 + sum(must_contain(c) * n for c, n in BAG_TREE[color].items())
 
 
 def main(args):
@@ -52,15 +53,21 @@ def main(args):
 
 def solve(file_path):
     """Solve the problem for one file path"""
+    # Reset cache
+    BAG_TREE.clear()
+    can_contain.cache_clear()
+    must_contain.cache_clear()
+
+    # Read input file
     print(f"\n{file_path}:")
-    bags = build_bag_tree(file_path.read_text().strip().split("\n"))
+    build_bag_tree(file_path.read_text().strip().split("\n"))
 
     # Part 1
-    colors_outside = len(can_contain(bags, color="shiny gold"))
+    colors_outside = len(can_contain(color="shiny gold"))
     print(f"{colors_outside} colors can eventually contain a shiny gold bag")
 
     # Part 2
-    bags_inside = must_contain(bags, color="shiny gold") - 1
+    bags_inside = must_contain(color="shiny gold") - 1
     print(f"{bags_inside} bags are required inside a shiny gold bag")
 
 
