6044b996d3317597e10cd2c0fe15fa3dcb474713
Geir Arne Hjelle <geirarne@gmail.com>
2020-12-13 00:20:59 +0100

    Update puzzle 11 2015

--- 2015/11_corporate_policy/aoc11.py
+++ 2015/11_corporate_policy/aoc11.py
@@ -4,12 +4,13 @@ Advent of Code 2015, day 11
 Solution by Geir Arne Hjelle, 2016-12-10
 """
 import itertools
+import pathlib
 import sys
 
 
 def increment_password(password):
-    if password.endswith('z'):
-        return increment_password(password[:-1]) + 'a'
+    if password.endswith("z"):
+        return increment_password(password[:-1]) + "a"
     return password[:-1] + chr(ord(password[-1]) + 1)
 
 
@@ -20,11 +21,17 @@ def generate_passwords(password):
 
 
 def next_password(seed):
-    return next(itertools.dropwhile(lambda p: not is_valid(p), generate_passwords(seed)))
+    return next(
+        itertools.dropwhile(lambda p: not is_valid(p), generate_passwords(seed))
+    )
 
 
 def is_valid(password):
-    return not contains_iol(password) and contains_straight(password) and contains_pairs(password)
+    return (
+        not contains_iol(password)
+        and contains_straight(password)
+        and contains_pairs(password)
+    )
 
 
 def contains_straight(password):
@@ -35,11 +42,11 @@ def contains_straight(password):
 
 
 def contains_iol(password):
-    return any(c in password for c in 'iol')
+    return any(c in password for c in "iol")
 
 
 def contains_pairs(password):
-    pair_idx = list()
+    pair_idx = []
     for idx, (c1, c2) in enumerate(zip(password[:-1], password[1:])):
         if c1 == c2:
             pair_idx.append(idx)
@@ -47,18 +54,24 @@ def contains_pairs(password):
     return pair_idx and max(pair_idx) - min(pair_idx) >= 2
 
 
-def main():
-    for filename in sys.argv[1:]:
-        print('\n{}:'.format(filename))
-        with open(filename, mode='r') as fid:
-            for line in fid:
-                password = line.strip()
-
-                print("Santa's current password is {}".format(password))
-                for _ in range(2):
-                    password = next_password(password)
-                    print("   Santa's next password is {}".format(password))
+def main(args):
+    """Solve the problem for all file paths"""
+    for file_path in [pathlib.Path(p) for p in args if not p.startswith("-")]:
+        solve(file_path)
 
 
-if __name__ == '__main__':
-    main()
+def solve(file_path):
+    """Solve the problem for one file path"""
+    print(f"\n{file_path}:")
+    with open(file_path, mode="r") as fid:
+        for line in fid:
+            password = line.strip()
+
+            print(f"Santa's current password is {password}")
+            for _ in range(2):
+                password = next_password(password)
+                print(f"   Santa's next password is {password}")
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
