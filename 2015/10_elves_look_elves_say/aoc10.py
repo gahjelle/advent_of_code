33ad363cd3e856b482dd36763331122b9fc62d80
Geir Arne Hjelle <geirarne@gmail.com>
2020-12-13 00:04:02 +0100

    Update puzzle 10 2015

--- 2015/10_elves_look_elves_say/aoc10.py
+++ 2015/10_elves_look_elves_say/aoc10.py
@@ -3,25 +3,34 @@
 Advent of Code 2015, day 10
 Solution by Geir Arne Hjelle, 2016-12-06
 """
+import pathlib
 import itertools
 import sys
 
 
 def look_and_say(sequence):
     while True:
-        sequence = ''.join(str(len(list(g[1]))) + g[0] for g in itertools.groupby(sequence))
+        sequence = "".join(
+            str(len(list(g[1]))) + g[0] for g in itertools.groupby(sequence)
+        )
         yield sequence
 
 
-def main():
-    for filename in sys.argv[1:]:
-        print('\n{}:'.format(filename))
-        with open(filename, mode='r') as fid:
-            for line in fid:
-                lengths = [len(s) for s in itertools.islice(look_and_say(line.strip()), 50)]
-                print('Length of {} after 40 iterations: {}'.format(line.strip(), lengths[39]))
-                print('Length of {} after 50 iterations: {}'.format(line.strip(), lengths[49]))
+def main(args):
+    """Solve the problem for all file paths"""
+    for file_path in [pathlib.Path(p) for p in args if not p.startswith("-")]:
+        solve(file_path)
 
 
-if __name__ == '__main__':
-    main()
+def solve(file_path):
+    """Solve the problem for one file path"""
+    print(f"\n{file_path}:")
+    with file_path.open(mode="r") as fid:
+        for line in fid:
+            lengths = [len(s) for s in itertools.islice(look_and_say(line.strip()), 50)]
+            print(f"Length of {line.strip()} after 40 iterations: {lengths[39]}")
+            print(f"Length of {line.strip()} after 50 iterations: {lengths[49]}")
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
