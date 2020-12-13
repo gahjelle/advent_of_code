5005374af4c1bb032136ee03e3b6cd0bdd6fdf78
Geir Arne Hjelle <geirarne@gmail.com>
2020-12-13 02:41:39 +0100

    Update puzzle 1 2016

--- 2016/01_no_time_for_a_taxi_cab/aoc01.py
+++ 2016/01_no_time_for_a_taxi_cab/aoc01.py
@@ -3,24 +3,27 @@
 Advent of Code 2016, day 1
 Solution by Geir Arne Hjelle, 2016-12-03
 """
+import pathlib
 import sys
 
-TURNS = {'N': {'R': 'E', 'L': 'W'},
-         'E': {'R': 'S', 'L': 'N'},
-         'S': {'R': 'W', 'L': 'E'},
-         'W': {'R': 'N', 'L': 'S'},
-        }
+TURNS = {
+    "N": {"R": "E", "L": "W"},
+    "E": {"R": "S", "L": "N"},
+    "S": {"R": "W", "L": "E"},
+    "W": {"R": "N", "L": "S"},
+}
 
-MOVES = {'N': lambda p: (p[0], p[1] + 1),
-         'E': lambda p: (p[0] + 1, p[1]),
-         'S': lambda p: (p[0], p[1] - 1),
-         'W': lambda p: (p[0] - 1, p[1]),
-        }
+MOVES = {
+    "N": lambda p: (p[0], p[1] + 1),
+    "E": lambda p: (p[0] + 1, p[1]),
+    "S": lambda p: (p[0], p[1] - 1),
+    "W": lambda p: (p[0] - 1, p[1]),
+}
 
 
 def find_distance(instructions):
     position = (0, 0)
-    direction = 'N'
+    direction = "N"
     visited = set()
 
     for instruction in instructions:
@@ -30,21 +33,31 @@ def find_distance(instructions):
         for _ in range(distance):
             position = MOVES[direction](position)
             if position in visited:
-                print('Already visited {} which is {} blocks away'
-                      ''.format(position, sum(abs(c) for c in position)))
+                print(
+                    "Already visited {} which is {} blocks away"
+                    "".format(position, sum(abs(c) for c in position))
+                )
             visited.add(position)
 
-    print('Ended at {} which is {} blocks away'
-          ''.format(position, sum(abs(c) for c in position)))
+    print(
+        "Ended at {} which is {} blocks away"
+        "".format(position, sum(abs(c) for c in position))
+    )
 
 
-def main():
-    for filename in sys.argv[1:]:
-        print('\n{}:'.format(filename))
-        with open(filename, mode='r') as fid:
-            for line in fid:
-                find_distance(i.strip() for i in line.split(','))
+def main(args):
+    """Solve the problem for all file paths"""
+    for file_path in [pathlib.Path(p) for p in args if not p.startswith("-")]:
+        solve(file_path)
 
 
-if __name__ == '__main__':
-    main()
+def solve(file_path):
+    """Solve the problem for one file path"""
+    print(f"\n{file_path}:")
+    with file_path.open(mode="r") as fid:
+        for line in fid:
+            find_distance(i.strip() for i in line.split(","))
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
