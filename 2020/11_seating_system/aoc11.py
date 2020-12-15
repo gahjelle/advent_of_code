798235a9640ce68f52b99da64fbe5a7b59f091e6
Geir Arne Hjelle <geirarne@gmail.com>
2020-12-15 23:26:11 +0100

    Use a graph for part 2. Speedup: 7s -> 1s

--- 2020/11_seating_system/aoc11.py
+++ 2020/11_seating_system/aoc11.py
@@ -4,13 +4,13 @@ Advent of Code 2020, day 11
 Solution by Geir Arne Hjelle, 2020-12-11
 """
 import enum
-import itertools
 import pathlib
 import sys
 import time
 
 import colorama
 import numpy as np
+from colorama import Cursor
 
 colorama.init(autoreset=True)
 debug = print if "--debug" in sys.argv else lambda *_: None
@@ -20,8 +20,8 @@ DIRECTIONS = ((-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 
 
 
 class Cell(enum.IntEnum):
-    FLOOR = 0
-    EMPTY = -1
+    FLOOR = -1
+    EMPTY = 0
     OCCUPIED = 1
 
 
@@ -33,11 +33,41 @@ CELL2MAP = {
     Cell.EMPTY: f"{colorama.Fore.GREEN}⎣",
     Cell.OCCUPIED: f"{colorama.Fore.RED}▌",
 }
-INLINE_NEIGHBORS = {}
 
 
 def parse_cells(lines):
     return add_border(np.array([[TEXT2CELL[c] for c in line] for line in lines]))
+
+
+def cells_to_graph(cells):
+    nodes, edges = {}, {}
+    rows, cols = cells.shape
+
+    for row in range(rows):
+        for col in range(cols):
+            if cells[row, col] == Cell.FLOOR:
+                continue
+
+            cell_num = row * cols + col
+            nodes[cell_num] = cells[row, col]
+            edges[cell_num] = find_los_neighbors(cells, row, col)
+
+    return nodes, edges
+
+
+def find_los_neighbors(cells, row, col):
+    rows, cols = cells.shape
+    neighbors = []
+
+    for dr, dc in DIRECTIONS:
+        cr, cc = row + dr, col + dc
+        while 0 <= cr < rows and 0 <= cc < cols:
+            if cells[cr, cc] != Cell.FLOOR:
+                neighbors.append(cr * cols + cc)
+                break
+            cr, cc = cr + dr, cc + dc
+
+    return neighbors
 
 
 def add_border(cells):
@@ -48,8 +78,20 @@ def add_border(cells):
 
 def draw_cells(cells):
     print(
-        f"{colorama.Cursor.POS(0, 4)}"
+        f"{Cursor.POS(0, 4)}"
         + "\n".join("".join(CELL2MAP[c] for c in row) for row in cells)
+    )
+
+
+def draw_nodes(nodes, cols):
+    def col_row(node):
+        return divmod(4 * cols + node, cols)[::-1]
+
+    print(
+        "".join(
+            f"{Cursor.POS(*col_row(n))}{Cursor.FORWARD()}{CELL2MAP[c]}"
+            for n, c in nodes.items()
+        )
     )
 
 
@@ -66,52 +108,18 @@ def adjacent_neighbors(cells):
     )
 
 
-def inline_neighbors(cells):
-    # Cache indices of neighbors for all cells
-    if not INLINE_NEIGHBORS:
-        for x, y in itertools.product(
-            range(1, cells.shape[0] - 1), range(1, cells.shape[1] - 1)
-        ):
-            if cells[x, y] == Cell.FLOOR:
-                continue
-            INLINE_NEIGHBORS[(x, y)] = find_inline_neighbors(cells, x, y)
-
-    # Calculate number of occupied neighbors
-    neighbors = np.zeros(cells.shape)
-    for (x, y), cell_nbs in INLINE_NEIGHBORS.items():
-        neighbors[x, y] = np.sum(cells[cell_nbs] == Cell.OCCUPIED)
-
-    return neighbors[1:-1, 1:-1]
-
-
-def find_inline_neighbors(cells, cell_x, cell_y):
-    neighbors_x, neighbors_y = [], []
-    max_x, max_y = [s - 1 for s in cells.shape]
-    for dx, dy in DIRECTIONS:
-        x, y = cell_x + dx, cell_y + dy
-        while cells[x, y] == Cell.FLOOR:
-            if x <= 0 or x >= max_x or y <= 0 or y >= max_y:
-                break
-            x, y = x + dx, y + dy
-        if cells[x, y] != Cell.FLOOR:
-            neighbors_x.append(x)
-            neighbors_y.append(y)
-
-    return neighbors_x, neighbors_y
-
-
-def simulate_until_stable(cells, count_neighbors, occupied_threshold):
+def simulate_cells(cells, occupied_threshold):
     while True:
         if "--draw" in sys.argv:
             draw_cells(cells)
             time.sleep(SLEEP)
 
-        cells, changed = next_generation(cells, count_neighbors, occupied_threshold)
+        cells, changed = next_cells(cells, adjacent_neighbors, occupied_threshold)
         if not changed:
             return cells
 
 
-def next_generation(cells, count_neighbors, occupied_threshold):
+def next_cells(cells, count_neighbors, occupied_threshold):
     neighbors = count_neighbors(cells)
 
     current = cells[1:-1, 1:-1]
@@ -124,6 +132,36 @@ def next_generation(cells, count_neighbors, occupied_threshold):
     return add_border(new_generation), np.any(new_generation != current)
 
 
+def simulate_nodes(nodes, edges, occupied_threshold, cols):
+    while True:
+        if "--draw" in sys.argv:
+            draw_nodes(nodes, cols)
+            time.sleep(SLEEP)
+
+        nodes, changed = next_nodes(nodes, edges, occupied_threshold)
+        if not changed:
+            return nodes
+
+
+def next_nodes(nodes, edges, occupied_threshold):
+    neighbors = {
+        n: (nodes[n], sum(nodes[nb] for nb in nbs)) for n, nbs in edges.items()
+    }
+    occupied = [n for n, (c, nbs) in neighbors.items() if c == Cell.EMPTY and nbs == 0]
+    for node in occupied:
+        nodes[node] = Cell.OCCUPIED
+
+    empty = [
+        n
+        for n, (c, nbs) in neighbors.items()
+        if c == Cell.OCCUPIED and nbs >= occupied_threshold
+    ]
+    for node in empty:
+        nodes[node] = Cell.EMPTY
+
+    return nodes, bool(occupied + empty)
+
+
 def main(args):
     """Solve the problem for all file paths"""
     for file_path in [pathlib.Path(p) for p in args if not p.startswith("-")]:
@@ -134,15 +172,16 @@ def solve(file_path):
     """Solve the problem for one file path"""
     print(f"\n{file_path}:")
     cells = parse_cells(file_path.read_text().strip().split("\n"))
+    graph = cells_to_graph(cells)
 
     # Part 1
-    part_1 = simulate_until_stable(cells, adjacent_neighbors, occupied_threshold=4)
+    part_1 = simulate_cells(cells, occupied_threshold=4)
     num_occupied = np.sum(part_1 == Cell.OCCUPIED)
     print(f"Occupied seats in the end: {num_occupied}")
 
     # Part 1
-    part_2 = simulate_until_stable(cells, inline_neighbors, occupied_threshold=5)
-    num_occupied = np.sum(part_2 == Cell.OCCUPIED)
+    part_2 = simulate_nodes(*graph, occupied_threshold=5, cols=cells.shape[1])
+    num_occupied = sum(part_2.values())
     print(f"Occupied seats in the end: {num_occupied}")
 
 
