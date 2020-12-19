0041be539e33989a1df44dce366ba765ae1ef998
Geir Arne Hjelle <geirarne@gmail.com>
2020-12-19 20:35:14 +0100

    Update puzzle 4 2016

--- 2016/04_security_through_obscurity/aoc04.py
+++ 2016/04_security_through_obscurity/aoc04.py
@@ -4,27 +4,33 @@ Advent of Code 2016, day 4
 Solution by Geir Arne Hjelle, 2016-12-04
 """
 # Standard library imports
+import pathlib
 import sys
+
+debug = print if "--debug" in sys.argv else lambda *_: None
 
 
 def _parse_string(string):
     checksum = string.strip()[-6:-1]
-    messages = string.strip()[:-7].split('-')
+    messages = string.strip()[:-7].split("-")
     sector_id = int(messages.pop(-1))
-    message = ' '.join(messages)
+    message = " ".join(messages)
 
     return message, sector_id, checksum
 
+
 def check_room(string):
     message, sector_id, checksum = _parse_string(string)
-    counter = {c: '{:04d}{}'.format(10000 - message.count(c), c) for c in set(message) if c != ' '}
-    actual_checksum = ''.join(sorted(counter, key=lambda c: counter[c]))[:5]
+    counter = {
+        c: f"{10000 - message.count(c):04d}{c}" for c in set(message) if c != " "
+    }
+    actual_checksum = "".join(sorted(counter, key=lambda c: counter[c]))[:5]
 
     return sector_id if checksum == actual_checksum else 0
 
 
 def decrypt_room(string):
-    letters = 'abcdefghijklmnopqrstuvwxyz'
+    letters = "abcdefghijklmnopqrstuvwxyz"
     message, sector_id, _ = _parse_string(string)
     decrypted = list()
 
@@ -33,20 +39,28 @@ def decrypt_room(string):
             idx = letters.index(c)
             decrypted.append(letters[(idx + sector_id) % 26])
         except ValueError:
-            decrypted.append(' ')
+            decrypted.append(" ")
 
-    return ''.join(decrypted), sector_id
+    return sector_id, "".join(decrypted)
 
 
-def main():
-    for filename in sys.argv[1:]:
-        print('\n{}:'.format(filename))
+def main(args):
+    """Solve the problem for all file paths"""
+    for file_path in [pathlib.Path(p) for p in args if not p.startswith("-")]:
+        solve(file_path)
 
-        with open(filename, mode='r') as fid:
-            messages = [decrypt_room(l) for l in fid if check_room(l)]
 
-        print('\n'.join('{m[0]} ({m[1]})'.format(m=m) for m in messages))
-        print('Sum of sector IDs is {}'.format(sum(m[1] for m in messages)))
+def solve(file_path):
+    """Solve the problem for one file path"""
+    print(f"\n{file_path}:")
 
-if __name__ == '__main__':
-    main()
+    with open(file_path, mode="r") as fid:
+        messages = [decrypt_room(ln) for ln in fid if check_room(ln)]
+    debug("\n".join(f"{m[0]}: {m[1]}" for m in sorted(messages)))
+
+    print(f"Sum of sector IDs is {sum(m[0] for m in messages)}")
+    print("\n".join(f"{m[1]} ({m[0]})" for m in messages if "northpole" in m[1]))
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
