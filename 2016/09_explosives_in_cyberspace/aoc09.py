ae759dc7ae683d3aae0230adfb164cf2ec50df4b
Geir Arne Hjelle <geirarne@gmail.com>
2020-12-20 23:18:08 +0100

    Update puzzle 9 2016

--- 2016/09_explosives_in_cyberspace/aoc09.py
+++ 2016/09_explosives_in_cyberspace/aoc09.py
@@ -5,30 +5,40 @@ Solution by Geir Arne Hjelle, 2016-12-09
 """
 # Standard library imports
 import itertools
+import pathlib
 import sys
 
 
 def find_length(string, iterate):
     s_iter = iter(string)
     for c in s_iter:
-        if c != '(':
+        if c != "(":
             yield 1
             continue
 
-        counter, repeats = [int(n) for n in ''.join(itertools.takewhile(lambda c: c != ')', s_iter)).split('x')]
+        counter, repeats = [
+            int(n)
+            for n in "".join(itertools.takewhile(lambda c: c != ")", s_iter)).split("x")
+        ]
         data = itertools.islice(s_iter, counter)
         yield repeats * (sum(find_length(data, True)) if iterate else len(list(data)))
 
 
-def main():
-    for filename in sys.argv[1:]:
-        print('\n{}:'.format(filename))
-        with open(filename, mode='r') as fid:
-            for line in fid:
-                string = line.strip()
-                print('{:<40s} - Method one: {}'.format(string[:40], sum(find_length(string, False))))
-                print('{:<40s} - Method two: {}'.format(string[:40], sum(find_length(string, True))))
+def main(args):
+    """Solve the problem for all file paths"""
+    for file_path in [pathlib.Path(p) for p in args if not p.startswith("-")]:
+        solve(file_path)
 
 
-if __name__ == '__main__':
-    main()
+def solve(file_path):
+    """Solve the problem for one file path"""
+    print(f"\n{file_path}:")
+    with open(file_path, mode="r") as fid:
+        for line in fid:
+            string = line.strip()
+            print(f"{string[:40]:<40s} - Method one: {sum(find_length(string, False))}")
+            print(f"{string[:40]:<40s} - Method two: {sum(find_length(string, True))}")
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
