705b59a98014b943c72181ca2f4b2809d9754793
Geir Arne Hjelle <geirarne@gmail.com>
2020-12-20 21:28:06 +0100

    Update puzzle 6 2016

--- 2016/06_signals_and_noise/aoc06.py
+++ 2016/06_signals_and_noise/aoc06.py
@@ -3,23 +3,31 @@
 Advent of Code 2016, day 6
 Solution by Geir Arne Hjelle, 2016-12-06
 """
+
 # Standard library imports
+import pathlib
 import sys
 
 
 def common_letters(words, most_common):
-    return ''.join(sorted(c, key=c.count, reverse=most_common)[0] for c in zip(*words))
+    return "".join(sorted(c, key=c.count, reverse=most_common)[0] for c in zip(*words))
 
 
-def main():
-    for filename in sys.argv[1:]:
-        print('\n{}:'.format(filename))
-        with open(filename, mode='r') as fid:
-            words = [w.strip() for w in fid]
-
-        print('Error-corrected message:    {}'.format(common_letters(words, True)))
-        print('Modified repetition code:   {}'.format(common_letters(words, False)))
+def main(args):
+    """Solve the problem for all file paths"""
+    for file_path in [pathlib.Path(p) for p in args if not p.startswith("-")]:
+        solve(file_path)
 
 
-if __name__ == '__main__':
-    main()
+def solve(file_path):
+    """Solve the problem for one file path"""
+    print(f"\n{file_path}:")
+    with file_path.open(mode="r") as fid:
+        words = [w.strip() for w in fid]
+
+    print(f"Error-corrected message:    {common_letters(words, True)}")
+    print(f"Modified repetition code:   {common_letters(words, False)}")
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
