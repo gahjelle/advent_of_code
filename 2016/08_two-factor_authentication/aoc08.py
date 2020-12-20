d6aba0bc741555510a4d94e7c03ec0ed4c98e7a1
Geir Arne Hjelle <geirarne@gmail.com>
2020-12-20 21:50:33 +0100

    Update puzzle 8 2016

--- 2016/08_two-factor_authentication/aoc08.py
+++ 2016/08_two-factor_authentication/aoc08.py
@@ -3,7 +3,9 @@
 Advent of Code 2016, day 8
 Solution by Geir Arne Hjelle, 2016-12-08
 """
+
 # Standard library imports
+import pathlib
 import sys
 
 # Third party imports
@@ -14,17 +16,17 @@ def set_up_lights(fid):
     lights = np.zeros((6, 50), dtype=bool)
     for line in fid:
         tokens = line.strip().split()
-        if line.startswith('init '):    # for testing
-            cols, rows = [int(c) for c in tokens[1].split('x')]
+        if line.startswith("init "):  # for testing
+            cols, rows = [int(c) for c in tokens[1].split("x")]
             lights = np.zeros((rows, cols), dtype=bool)
-        elif line.startswith('rect '):
-            cols, rows = [int(c) for c in tokens[1].split('x')]
+        elif line.startswith("rect "):
+            cols, rows = [int(c) for c in tokens[1].split("x")]
             lights[:rows, :cols] = True
-        elif line.startswith('rotate column x='):
+        elif line.startswith("rotate column x="):
             col = int(tokens[2][2:])
             offset = int(tokens[4])
             lights[:, col] = np.hstack((lights[-offset:, col], lights[:-offset, col]))
-        elif line.startswith('rotate row y='):
+        elif line.startswith("rotate row y="):
             row = int(tokens[2][2:])
             offset = int(tokens[4])
             lights[row, :] = np.hstack((lights[row, -offset:], lights[row, :-offset]))
@@ -34,16 +36,23 @@ def set_up_lights(fid):
 
 def display_lights(lights):
     for row in lights:
-        print('    |' + ''.join(('#' if l else ' ' for l in row)) + '|')
+        print("    |" + "".join(("#" if light else " " for light in row)) + "|")
 
 
-def main():
-    for filename in sys.argv[1:]:
-        print('\n{}:'.format(filename))
-        with open(filename, mode='r') as fid:
-            lights = set_up_lights(fid)
-        print('{} lights are lit'.format(np.sum(lights)))
-        display_lights(lights)
+def main(args):
+    """Solve the problem for all file paths"""
+    for file_path in [pathlib.Path(p) for p in args if not p.startswith("-")]:
+        solve(file_path)
 
-if __name__ == '__main__':
-    main()
+
+def solve(file_path):
+    """Solve the problem for one file path"""
+    print(f"\n{file_path}:")
+    with file_path.open(mode="r") as fid:
+        lights = set_up_lights(fid)
+    print(f"{np.sum(lights)} lights are lit")
+    display_lights(lights)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
