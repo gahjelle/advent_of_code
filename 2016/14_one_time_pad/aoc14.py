4e67749c434c6c4fd33bea8ac62bd784ff75bb49
Geir Arne Hjelle <geirarne@gmail.com>
2020-12-21 22:21:36 +0100

    Update puzzle 14 2016

--- 2016/14_one_time_pad/aoc14.py
+++ 2016/14_one_time_pad/aoc14.py
@@ -6,20 +6,21 @@ Solution by Geir Arne Hjelle, 2016-12-14
 # Standard library imports
 import hashlib
 import itertools
+import pathlib
 import sys
 
 
 def generate_md5_numbers(salt, stretch=0):
     for counter in itertools.count(start=0, step=1):
         if not counter % 1000:
-            print('.', end='', flush=True)
+            print(".", end="", flush=True)
         md5 = hashlib.md5()
-        md5.update(bytes(salt + str(counter), encoding='utf-8'))
+        md5.update(bytes(salt + str(counter), encoding="utf-8"))
 
-        for i in range(stretch):
+        for _ in range(stretch):
             digest = md5.hexdigest()
             md5 = hashlib.md5()
-            md5.update(bytes(digest, encoding='utf-8'))
+            md5.update(bytes(digest, encoding="utf-8"))
 
         digest = md5.hexdigest()
         counts = [(c, len(list(g))) for c, g in itertools.groupby(digest)]
@@ -27,19 +28,22 @@ def generate_md5_numbers(salt, stretch=0):
             yield counter, c, lg
             break
         for c, lg in [(c, lg) for c, lg in counts if lg >= 3]:
-#            print(counter, digest, c, lg)
             yield counter, c, lg
             break
+
 
 def find_onetime_pads(salt, stretch=0):
     candidates = dict()
     onetime_pads = set()
     gen_nums = iter(generate_md5_numbers(salt, stretch))
+    counter = 0
 
     while len(onetime_pads) < 64 or counter < sorted(onetime_pads)[63] + 1000:
         counter, char, count = next(gen_nums)
         if char in candidates:
-            candidates[char] = [idx for idx in candidates[char] if counter - 1000 <= idx < counter]
+            candidates[char] = [
+                idx for idx in candidates[char] if counter - 1000 <= idx < counter
+            ]
         if count >= 5:
             [onetime_pads.add(candidate) for candidate in candidates[char]]
         candidates.setdefault(char, list()).append(counter)
@@ -47,14 +51,23 @@ def find_onetime_pads(salt, stretch=0):
     return max(sorted(onetime_pads)[:64])
 
 
-def main():
-    for filename in sys.argv[1:]:
-        print('\n{}:'.format(filename))
-        with open(filename, mode='r') as fid:
-            for line in fid:
-                print('   Index {} gives the 64th key'.format(find_onetime_pads(line.strip())))
-                print('   Index {} gives the 64th key with stretching'.format(find_onetime_pads(line.strip(), 2016)))
+def main(args):
+    """Solve the problem for all file paths"""
+    for file_path in [pathlib.Path(p) for p in args if not p.startswith("-")]:
+        solve(file_path)
 
 
-if __name__ == '__main__':
-    main()
+def solve(file_path):
+    """Solve the problem for one file path"""
+    print(f"\n{file_path}:")
+    with file_path.open(mode="r") as fid:
+        for line in fid:
+            salt = line.strip()
+            print(f" Index {find_onetime_pads(salt)} gives the 64th key")
+            print(
+                f" Index {find_onetime_pads(salt, 2016)} gives the stretched 64th key"
+            )
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
