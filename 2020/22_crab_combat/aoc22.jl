379c3b14e7f40044eea135b5fe475dc0f9d54981
Geir Arne Hjelle <geirarne@gmail.com>
2020-12-22 07:44:54 +0100

    Puzzle 22 2020

---
+++ 2020/22_crab_combat/aoc22.jl
@@ -0,0 +1,85 @@
+# Crab Combat
+#
+# Advent of Code 2020, day 22
+# Solution by Geir Arne Hjelle, 2020-12-22
+
+using Pipe
+
+function parse_cards(lines)
+    @pipe lines |> split(_, "\n") |> _[2:end] |> map(n -> parse(Int, n), _)
+end
+
+
+function regular_combat(deck1, deck2)
+    while !isempty(deck1) && !isempty(deck2)
+        card1 = popfirst!(deck1)
+        card2 = popfirst!(deck2)
+
+        if card1 > card2
+            append!(deck1, [card1, card2])
+        else
+            append!(deck2, [card2, card1])
+        end
+    end
+
+    vcat(deck1, deck2)
+end
+
+
+function recursive_combat(deck1, deck2)
+    seen = Set()
+    while !isempty(deck1) && !isempty(deck2)
+        if (deck1, deck2) ∈ seen
+            return (winning_deck = deck1, winner = 1)
+        end
+        push!(seen, (deck1 |> copy, deck2 |> copy))
+
+        card1 = popfirst!(deck1)
+        card2 = popfirst!(deck2)
+
+        if card1 <= length(deck1) && card2 <= length(deck2)
+            winning_deck, winner = recursive_combat(deck1[1:card1] |> copy, deck2[1:card2] |> copy)
+            if winner == 1
+                append!(deck1, [card1, card2])
+            else
+                append!(deck2, [card2, card1])
+            end
+        else
+            if card1 > card2
+                append!(deck1, [card1, card2])
+            else
+                append!(deck2, [card2, card1])
+            end
+        end
+    end
+    (winning_deck = vcat(deck1, deck2), winner = isempty(deck2) ? 1 : 2)
+end
+
+
+function score_deck(num_cards)
+    function _score_deck(deck)
+        @pipe zip(deck, num_cards:-1:1) |> map(c -> prod(c), _) |> sum
+    end
+end
+
+
+# Solve the problem for one file
+function solve(filename)
+    println("\n$(filename)")
+
+    # Read from file
+    deck1, deck2 = open(filename) do fid
+        @pipe fid |> readlines |> join(_, "\n") |> split(_, "\n\n") |> map(c -> parse_cards(c), _)
+    end
+    num_cards = length(deck1) + length(deck2)
+
+    # Part 1
+    regular_combat(deck1 |> copy, deck2 |> copy) |> score_deck(num_cards) |> println
+
+    # Part 2
+    @pipe recursive_combat(deck1 |> copy, deck2 |> copy) |> _.winning_deck |> score_deck(num_cards) |> println
+end
+
+
+# Solve the problem for each file
+ARGS .|> solve
