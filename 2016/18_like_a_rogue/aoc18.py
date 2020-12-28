ad3b0f09625e893a6f81c313987b01714075c4ba
Geir Arne Hjelle <geirarne@gmail.com>
2020-12-28 05:00:22 +0100

    Update puzzle 18 2016

--- 2016/18_like_a_rogue/aoc18.py
+++ 2016/18_like_a_rogue/aoc18.py
@@ -1,20 +1,22 @@
-"""Like a rogue
+"""Like a Rogue
 
 Advent of Code 2016, day 18
 Solution by Geir Arne Hjelle, 2017-05-28
 """
+
 # Standard library imports
+import pathlib
 import sys
 
-TRAPS = {'^^.', '.^^', '^..', '..^'}
+TRAPS = {"^^.", ".^^", "^..", "..^"}
 
 
 def next_row(row):
     new_tiles = list()
-    for tiles in zip('.' + row, row, row[1:] + '.'):
-        new_tiles.append('^' if ''.join(tiles) in TRAPS else '.')
+    for tiles in zip("." + row, row, row[1:] + "."):
+        new_tiles.append("^" if "".join(tiles) in TRAPS else ".")
 
-    return ''.join(new_tiles)
+    return "".join(new_tiles)
 
 
 def map_room(first_row, num_rows):
@@ -22,19 +24,28 @@ def map_room(first_row, num_rows):
     while len(rows) < num_rows:
         rows.append(next_row(rows[-1]))
 
-    if num_rows < 100:
-        print('\n'.join(rows))
-    print('{} safe tiles in {} rows'.format(sum(r.count('.') for r in rows), num_rows))
+    if "--draw" in sys.argv and num_rows < 1000:
+        print("\n".join(rows))
+
+    return rows
 
 
-def main():
-    for filename in sys.argv[1:]:
-        print('\n{}:'.format(filename))
-        with open(filename, mode='r') as fid:
-            for line in fid:
-                num_rows, first_row = line.strip().split()
-                map_room(first_row, int(num_rows))
+def main(args):
+    """Solve the problem for all file paths"""
+    for file_path in [pathlib.Path(p) for p in args if not p.startswith("-")]:
+        solve(file_path)
 
 
-if __name__ == '__main__':
-    main()
+def solve(file_path):
+    """Solve the problem for one file path"""
+    print(f"\n{file_path}:")
+    with file_path.open(mode="r") as fid:
+        for line in fid:
+            num_rows, first_row = line.strip().split()
+            rows = map_room(first_row, int(num_rows))
+            num_safe = sum(r.count(".") for r in rows)
+            print(f"{num_safe} safe tiles in {num_rows} rows")
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
