1e347bc10dbfa23dcec859bcc0eec118cf854ede
Geir Arne Hjelle <geirarne@gmail.com>
2020-12-28 05:12:30 +0100

    Update puzzle 20 2016

--- 2016/20_firewall_rules/aoc20.py
+++ 2016/20_firewall_rules/aoc20.py
@@ -1,14 +1,19 @@
-"""Firewall rules
+"""Firewall Rules
 
 Advent of Code 2016, day 20
 Solution by Geir Arne Hjelle, 2017-05-28
 """
+
 # Standard library imports
+import pathlib
 import sys
 
 
 def read_blacklist(fid):
-    return sorted((int(first), int(last)) for first, last in [line.strip().split('-') for line in fid])
+    return sorted(
+        (int(first), int(last))
+        for first, last in [line.strip().split("-") for line in fid]
+    )
 
 
 def find_lowest(blacklist):
@@ -33,16 +38,25 @@ def count_allowed(blacklist):
         if last >= lowest:
             lowest = last + 1
 
-    return count, lowest - 1   # Does not account for any IPs at the end of the interval
+    return count, lowest - 1  # Does not account for any IPs at the end of the interval
 
 
-def main():
-    for filename in sys.argv[1:]:
-        print('\n{}:'.format(filename))
-        with open(filename, mode='r') as fid:
-            blacklist = read_blacklist(fid)
-            print('Lowest available IP: {}'.format(find_lowest(blacklist)))
-            print('Number of available IPs: {} (up to {})'.format(*count_allowed(blacklist)))
+def main(args):
+    """Solve the problem for all file paths"""
+    for file_path in [pathlib.Path(p) for p in args if not p.startswith("-")]:
+        solve(file_path)
 
-if __name__ == '__main__':
-    main()
+
+def solve(file_path):
+    """Solve the problem for one file path"""
+    print(f"\n{file_path}:")
+    with file_path.open(mode="r") as fid:
+        blacklist = read_blacklist(fid)
+        print(f"Lowest available IP: {find_lowest(blacklist)}")
+
+        count, lowest = count_allowed(blacklist)
+        print(f"Number of available IPs: {count} (up to {lowest})")
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
