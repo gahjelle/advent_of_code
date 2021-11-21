7711fa55fc6896edb85df8d9efb84bf20c0083ea
Geir Arne Hjelle <geirarne@gmail.com>
2021-11-21 22:43:15 +0100

    Use Enum.take_every for Robo-Santa

--- elixir/lib/2015/03_perfectly_spherical_houses_in_a_vacuum/aoc201503.ex
+++ elixir/lib/2015/03_perfectly_spherical_houses_in_a_vacuum/aoc201503.ex
@@ -6,13 +6,6 @@ defmodule AOC2015.Day03 do
 
   @steps %{"^" => {0, 1}, ">" => {1, 0}, "v" => {0, -1}, "<" => {-1, 0}}
 
-  def step({dx, dy}, {pos, {x, y}}), do: {MapSet.put(pos, {x + dx, y + dy}), {x + dx, y + dy}}
-  def visit(houses), do: houses |> Enum.reduce({MapSet.new([{0, 0}]), {0, 0}}, &step/2) |> elem(0)
-
-  def every_second([]), do: []
-  def every_second([first]), do: [first]
-  def every_second([first, _second | rest]), do: [first | every_second(rest)]
-
   def parse(puzzle_input) do
     puzzle_input |> String.split("", trim: true) |> Enum.map(fn c -> @steps[c] end)
   end
@@ -22,9 +15,17 @@ defmodule AOC2015.Day03 do
   end
 
   def part2(input) do
-    santa = input |> every_second() |> visit()
-    robosanta = input |> tl |> every_second() |> visit()
+    santa = input |> Enum.take_every(2) |> visit()
+    robosanta = input |> tl |> Enum.take_every(2) |> visit()
     MapSet.union(santa, robosanta) |> Enum.count()
+  end
+
+  defp visit(houses) do
+    houses
+    |> Enum.reduce({MapSet.new([{0, 0}]), 0, 0}, fn {dx, dy}, {pos, x, y} ->
+      {MapSet.put(pos, {x + dx, y + dy}), x + dx, y + dy}
+    end)
+    |> elem(0)
   end
 
   def main(args) do
