82fd4b6e6feef8052b3b58334a3b2dc70526fe3d
Geir Arne Hjelle <geirarne@gmail.com>
2022-12-08 13:32:52 +0100

    Clean up puzzle 8, 2022 in Python

--- python/2022/08_treetop_tree_house/aoc202208.py
+++ python/2022/08_treetop_tree_house/aoc202208.py
@@ -8,76 +8,80 @@ import sys
 
 def parse_data(puzzle_input):
     """Parse input."""
-    heights_row_col = [
+    trees_row_col = [
         [int(height) for height in line] for line in puzzle_input.split("\n")
     ]
-    heights_col_row = [list(col) for col in zip(*heights_row_col)]
-    return heights_row_col, heights_col_row
+    trees_col_row = [list(col) for col in zip(*trees_row_col)]
+    return trees_row_col, trees_col_row
 
 
-def part1(heights):
+def part1(trees):
     """Solve part 1."""
-    heights_rc, heights_cr = heights
-    rows, cols = len(heights_rc), len(heights_cr)
+    trees_rc, trees_cr = trees
+    rows, cols = len(trees_rc), len(trees_cr)
     return sum(
-        is_visible(row, col, heights_rc[row], heights_cr[col])
+        is_visible(row, col, trees_rc[row], trees_cr[col])
         for row in range(rows)
         for col in range(cols)
     )
 
 
-def part2(heights):
+def part2(trees):
     """Solve part 2."""
-    heights_rc, heights_cr = heights
-    rows, cols = len(heights_rc), len(heights_cr)
+    trees_rc, trees_cr = trees
+    rows, cols = len(trees_rc), len(trees_cr)
     return max(
-        scenic_score(row, col, heights_rc[row], heights_cr[col])
+        scenic_score(row, col, trees_rc[row], trees_cr[col])
         for row in range(rows)
         for col in range(cols)
     )
 
 
-def is_visible(idx_r, idx_c, row, col):
-    """Check if tree is visible from any size.
+def lines(row, col, tree_row, tree_col):
+    """Yield all four lines from a tree: up, down, left, right.
+
+    ## Example:
+
+    >>> list(lines(2, 1, [5, 4, 2, 4], [4, 3, 4, 5]))
+    [[4, 3, 4], [4, 5], [4, 5], [4, 2, 4]]
+    """
+    yield from [tree_col[row::-1], tree_col[row:], tree_row[col::-1], tree_row[col:]]
+
+
+def is_visible(row, col, tree_row, tree_col):
+    """Check if a tree is visible from any side.
 
     ## Examples:
 
-    >>> is_visible(0, 0, [1, 2, 4, 5], [4, 4, 4, 4])
+    >>> is_visible(0, 0, [4, 2, 9, 5], [4, 4, 4, 4])
     True
-    >>> is_visible(2, 1, [4, 3, 4, 5], [5, 4, 2, 4])
+    >>> is_visible(2, 1, [5, 4, 2, 4], [4, 3, 4, 5])
     False
     """
-    height = row[idx_c]
-    return (
-        all(h < height for h in row[:idx_c])
-        or all(h < height for h in row[idx_c + 1 :])
-        or all(h < height for h in col[:idx_r])
-        or all(h < height for h in col[idx_r + 1 :])
+    height = tree_row[col]
+    return any(
+        all(h < height for h in trees[1:])
+        for trees in lines(row, col, tree_row, tree_col)
     )
 
 
-def scenic_score(idx_r, idx_c, row, col):
-    """Check if tree is visible from any size.
+def scenic_score(row, col, tree_row, tree_col):
+    """Calculate the view from a tree.
 
     ## Examples:
 
-    >>> scenic_score(0, 0, [1, 2, 4, 5], [4, 4, 4, 4])
+    >>> scenic_score(0, 0, [1, 2, 4, 5], [1, 4, 4, 4])
     0
     >>> scenic_score(1, 2, [4, 5, 6, 7], [8, 6, 4, 2])
     4
     """
-    height = row[idx_c]
+    height = tree_row[col]
     return math.prod(
-        [
-            _score_line(height, row[idx_c::-1]),
-            _score_line(height, row[idx_c:]),
-            _score_line(height, col[idx_r::-1]),
-            _score_line(height, col[idx_r:]),
-        ]
+        _score_line(height, trees) for trees in lines(row, col, tree_row, tree_col)
     )
 
 
-def _score_line(height, heights):
+def _score_line(height, trees):
     """Score one sight line.
 
     ## Examples:
@@ -92,8 +96,8 @@ def _score_line(height, heights):
     3
     """
     return next(
-        (score for score, hgt in enumerate(heights[1:], start=1) if hgt >= height),
-        len(heights) - 1,
+        (score for score, h in enumerate(trees[1:], start=1) if h >= height),
+        len(trees) - 1,
     )
 
 
