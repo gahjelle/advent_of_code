88f1fde6dc659777297a25e6dc624d5489bffe0a
Geir Arne Hjelle <geirarne@gmail.com>
2022-12-14 21:45:34 +0100

    Improve runtime of puzzle 12, 2022 in Elixir

--- elixir/lib/2022/12_hill_climbing_algorithm/aoc202212.ex
+++ elixir/lib/2022/12_hill_climbing_algorithm/aoc202212.ex
@@ -23,21 +23,16 @@ defmodule AOC2022.Day12 do
   @doc """
   Solve part 1.
   """
-  def part1({heights, start, goal}), do: heights |> find_path(start, goal)
+  def part1({heights, start, goal}), do: heights |> find_path([start], goal)
 
   @doc """
   Solve part 2.
-
-  This is a suboptimal solution as it tries all possible startpoints. A better
-  solution would be to begin exploring at the target square and move backwards.
   """
   def part2({heights, _, goal}) do
     heights
     |> Map.filter(fn {_, height} -> height == 0 end)
     |> Map.keys()
-    |> Task.async_stream(&find_path(heights, &1, goal))
-    |> Stream.map(fn {:ok, num_steps} -> num_steps end)
-    |> Enum.min()
+    |> then(&find_path(heights, &1, goal))
   end
 
   @doc """
@@ -53,11 +48,11 @@ defmodule AOC2022.Day12 do
       ...>     {0, 5} => 13, {0, 6} => 12, {1, 0} => 1, {1, 1} => 17, {1, 2} => 5,
       ...>     {1, 3} => 6, {1, 4} => 18, {1, 5} => 10, {1, 6} => 11, {2, 0} => 2,
       ...>     {2, 1} => 3, {2, 2} => 4, {2, 3} => 7, {2, 4} => 8, {2, 5} => 9, {2, 6} => 23}
-      iex> find_path(heights, {0, 0}, {0, 1})
+      iex> find_path(heights, [{0, 0}], {0, 1})
       17
   """
   def find_path(heights, start, goal),
-    do: find_path(heights, [{0, start}], %{start => 0}, goal)
+    do: find_path(heights, Enum.map(start, &{0, &1}), Map.from_keys(start, 0), goal)
 
   def find_path(_, [{num_steps, goal} | _], _, goal), do: num_steps
   def find_path(_, [], _, _), do: nil
