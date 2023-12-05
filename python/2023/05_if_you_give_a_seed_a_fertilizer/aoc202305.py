adc16aaf356742f1816a40cfd77b8792015c66cd
Geir Arne Hjelle <geirarne@gmail.com>
2023-12-05 13:01:39 +0100

    Map ranges for easier logic in Puzzle 5, 2023 in Python

--- python/2023/05_if_you_give_a_seed_a_fertilizer/aoc202305.py
+++ python/2023/05_if_you_give_a_seed_a_fertilizer/aoc202305.py
@@ -6,93 +6,99 @@ import sys
 
 
 def parse_data(puzzle_input):
-    """Parse input."""
+    """Parse input.
+
+    Convert all ranges to use the notation [start, stop) instead of start and
+    length. This makes the interval math clearer at the end.
+    """
     blocks = [lines.split("\n") for lines in puzzle_input.split("\n\n")]
     seeds = [int(seed) for seed in blocks[0][0].split()[1:]]
-    maps = {}
-    for lines in blocks[1:]:
-        names = tuple(lines[0].split()[0].split("-")[:3:2])
-        maps[names] = [[int(n) for n in line.split()] for line in lines[1:]]
-    return seeds, maps
+    transforms = [
+        [
+            (src, src + num, dst - src)  # Map to start, stop, offset
+            for dst, src, num in [[int(n) for n in line.split()] for line in lines[1:]]
+        ]
+        for lines in blocks[1:]
+    ]
+    return seeds, transforms
 
 
 def part1(data):
     """Solve part 1."""
-    seeds, maps = data
-    locations = plant_seeds([(seed, 1) for seed in seeds], maps)
+    seeds, transforms = data
+    locations = plant_seed_ranges([(seed, seed + 1) for seed in seeds], transforms)
     return min(locations)[0]
 
 
 def part2(data):
     """Solve part 2."""
-    seeds, maps = data
-    locations = plant_seeds(list(zip(seeds[::2], seeds[1::2])), maps)
+    seeds, transforms = data
+    locations = plant_seed_ranges(
+        [(seed, seed + num) for seed, num in zip(seeds[::2], seeds[1::2])], transforms
+    )
     return min(locations)[0]
 
 
-def plant_seeds(seeds, maps, state="seed", to_state="location"):
-    """Plant seeds.
+def plant_seed_ranges(seeds, transforms, state="seed", to_state="location"):
+    """Plant ranges of seeds.
 
     ## Example:
 
-    >>> maps = {("seed", "location"): [[20, 70, 8], [60, 20, 5], [40, 44, 3]]}
-    >>> plant_seeds([(23, 1), (46, 2), (77, 3)], maps)
-    [(63, 1), (42, 1), (47, 1), (27, 1), (78, 2)]
+    >>> transforms = [[(70, 78, -50), (20, 25, 40), (44, 47, -4)]]
+    >>> plant_seed_ranges([(23, 24), (46, 48), (77, 80)], transforms)
+    [(63, 64), (42, 43), (47, 48), (27, 28), (78, 80)]
     """
-    transforms = dict(maps.keys())
-    while state != to_state:
-        next_state = transforms[state]
+    for transform in transforms:
         seeds = [
             new_seed
             for seed, num in seeds
-            for new_seed in grow_range(seed, num, maps[state, next_state])
+            for new_seed in grow_range(seed, num, transform)
         ]
-        state = next_state
 
     return seeds
 
 
-def grow_range(seed, num_seeds, tables):
+def grow_range(seed_start, seed_stop, transform):
     """Grow a range of seeds one stage.
 
     This may split the range into several ranges.
 
     ## Examples:
 
-    >>> tables = [[20, 70, 8], [60, 20, 5], [40, 44, 3]]
-    >>> grow_range(22, 3, tables)
-    [(62, 3)]
-    >>> grow_range(10, 10, tables)
-    [(10, 10)]
-    >>> grow_range(11, 11, tables)
-    [(11, 9), (60, 2)]
-    >>> grow_range(77, 3, tables)
-    [(27, 1), (78, 2)]
+    >>> transform = [(70, 78, -50), (20, 25, 40), (44, 47, -4)]
+    >>> grow_range(22, 25, transform)
+    [(62, 65)]
+    >>> grow_range(10, 20, transform)
+    [(10, 20)]
+    >>> grow_range(11, 22, transform)
+    [(11, 20), (60, 62)]
+    >>> grow_range(77, 80, transform)
+    [(27, 28), (78, 80)]
+    >>> grow_range(18, 29, transform)
+    [(18, 20), (60, 65), (25, 29)]
     """
-    if num_seeds < 1:
+    if seed_stop <= seed_start:
         return []
 
-    for dest, source, num in tables:
-        offset = seed - source
+    for start, stop, offset in transform:
+        # Fully contained in the current interval
+        if start <= seed_start < stop and start < seed_stop <= stop:
+            return [(seed_start + offset, seed_stop + offset)]
 
-        # Fully contained in current table
-        if 0 <= offset < num and 0 < offset + num_seeds <= num:
-            return [(dest + offset, num_seeds)]
-
-        # No overlap with current table
-        if offset + num_seeds <= 0 or offset >= num:
+        # No overlap with current interval
+        if seed_stop <= start or seed_start >= stop:
             continue
 
         # The general case, a range needs to be split in three parts:
-        # - before and after the current table is sent of to separate processing
-        # - the intersection with the current table is handled
+        # - before and after the current interval is sent of to separate processing
+        # - the intersection with the current interval is handled
         return (
-            grow_range(seed, -offset, tables)
-            + [(dest + max(offset, 0), min(num - offset, num, num_seeds + offset))]
-            + grow_range(source + num, num_seeds + offset - num, tables)
+            grow_range(seed_start, start, transform)
+            + [(max(seed_start, start) + offset, min(seed_stop, stop) + offset)]
+            + grow_range(stop, seed_stop, transform)
         )
 
-    return [(seed, num_seeds)]
+    return [(seed_start, seed_stop)]
 
 
 def solve(puzzle_input):
