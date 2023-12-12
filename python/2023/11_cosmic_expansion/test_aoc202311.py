efb5daef0b51af1b779011f1bd4eb534183467c5
Geir Arne Hjelle <geirarne@gmail.com>
2023-12-12 12:58:11 +0100

    Clean up for puzzle 11, 2023 in Python

--- python/2023/11_cosmic_expansion/test_aoc202311.py
+++ python/2023/11_cosmic_expansion/test_aoc202311.py
@@ -16,12 +16,6 @@ def example1():
     return aoc202311.parse_data(puzzle_input)
 
 
-@pytest.fixture
-def example2():
-    puzzle_input = (PUZZLE_DIR / "example2.txt").read_text().rstrip()
-    return aoc202311.parse_data(puzzle_input)
-
-
 def test_parse_example1(example1):
     """Test that input is parsed properly."""
     assert example1 == {
