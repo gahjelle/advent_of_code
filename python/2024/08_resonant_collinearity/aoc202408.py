3cf43e9cf313f9838df4781c0c4500091ac02f96
Geir Arne Hjelle <geirarne@gmail.com>
2024-12-08 09:01:47 +0100

    Clean up puzzle 8, 2024 in Python

--- python/2024/08_resonant_collinearity/aoc202408.py
+++ python/2024/08_resonant_collinearity/aoc202408.py
@@ -3,7 +3,6 @@
 # Standard library imports
 import collections
 import itertools
-import math
 import pathlib
 import sys
 
@@ -25,50 +24,33 @@ def parse_data(puzzle_input):
 def part1(data):
     """Solve part 1."""
     grid, antennas = data
-    return len(
-        grid
-        & set.union(*[find_antinodes(positions) for positions in antennas.values()])
-    )
+    return len(set.union(*[find_antinodes(grid, pos) for pos in antennas.values()]))
 
 
 def part2(data):
     """Solve part 2."""
     grid, antennas = data
-    return len(
-        set.union(
-            *[find_all_antinodes(grid, positions) for positions in antennas.values()]
-        )
-    )
+    return len(set.union(*[find_all_antinodes(grid, pos) for pos in antennas.values()]))
 
 
-def find_antinodes(antennas):
+def find_antinodes(grid, antennas):
     """Find the first resonant antinodes to the given antennas"""
-    return set.union(
-        *[
-            {
-                (2 * row_1 - row_2, 2 * col_1 - col_2),
-                (2 * row_2 - row_1, 2 * col_2 - col_1),
-            }
-            for (row_1, col_1), (row_2, col_2) in itertools.combinations(antennas, r=2)
-        ]
-    )
+    return grid & {
+        (2 * row - row_other, 2 * col - col_other)
+        for (row, col), (row_other, col_other) in itertools.permutations(antennas, r=2)
+    }
 
 
 def find_all_antinodes(grid, antennas):
-    """Find all resonant antinodes to the given antennas
-
-    Sort antennas to guarantee that row <= row_2. The input data are constructed
-    such that row1 != row2, so it's possible to loop over rows.
-    """
-    max_row, _ = max(grid)
+    """Find all resonant antinodes to the given antennas"""
     antinodes = set()
-    for (row, col), (row_2, col_2) in itertools.combinations(sorted(antennas), r=2):
-        dr, dc = row_2 - row, col_2 - col
+    for (row, col), (row_other, col_other) in itertools.permutations(antennas, r=2):
+        dr, dc = row_other - row, col_other - col
         # dr, dc = dr // math.gcd(dr, dc), dc // math.gcd(dr, dc)  # Not necessary
-        antinodes |= grid & {
-            (row + n * dr, col + n * dc)
-            for n in range(-row // dr, (max_row - row) // dr + 1)
-        }
+        for n in itertools.count(1):
+            if (antinode := (row + n * dr, col + n * dc)) not in grid:
+                break
+            antinodes.add(antinode)
     return antinodes
 
 
